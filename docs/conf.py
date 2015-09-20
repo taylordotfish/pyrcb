bb062d7c63f6863a3c6a54d2fafc4eb54b29e298
taylor.fish <contact@taylor.fish>
2015-09-20 09:04:33 -0700

    Set date in docs

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ language = None
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
-# today = ''
+today = 'September 18, 2015'
 # Else, today_fmt is used as the format for a strftime call.
 # today_fmt = '%B %d, %Y'
 
