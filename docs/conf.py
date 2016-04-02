696ff772f510dfa487ce05bcd070b635456fdd9e
taylor.fish <contact@taylor.fish>
2016-04-01 20:59:11 -0700

    Link to Python 3.5 docs

--- docs/conf.py
+++ docs/conf.py
@@ -297,7 +297,7 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3.4/', None),
+    'python': ('https://docs.python.org/3.5/', None),
     'py2': ('https://docs.python.org/2.7/', None),
 }
 
