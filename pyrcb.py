a77151a288a0698deb1ccdeaf64899b2e38c1b26
taylor.fish <contact@taylor.fish>
2016-10-14 21:32:25 -0700

    Update version to 1.14.0

--- pyrcb.py
+++ pyrcb.py
@@ -37,7 +37,7 @@ import traceback
 import time
 import warnings
 
-__version__ = "1.13.1"
+__version__ = "1.14.0"
 
 # ustr is unicode in Python 2 (because of unicode_literals)
 # and str in Python 3.
